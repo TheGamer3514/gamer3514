92464b1d420917a259a5c43aef65b2fd13cfe91b
thegamer3514 <thegamer3514@gmail.com>
2023-06-03 19:25:13 +0100

    all utility commands

--- src/gamer3514/api.py
+++ src/gamer3514/api.py
@@ -35,3 +35,46 @@ def profanitycheck(text: str, apikey: str):
         else:
             return result[0]['Profanity']
     raise Exception("Unknown error occured, please contact support")
+def texttobraille(text: str):
+    """
+    Convert text to braille.\n
+    Parameters:\n
+    • text: The text you want to convert\n\n
+    """
+    url=f"https://api.sillychat.tech/texttobraille?text={text}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        if result[0]['Error'] == 'True':
+            raise Exception(result[0]['ErrorInfo'])
+        else:
+            return result[0]['Braille']
+    raise Exception("Unknown error occured, please contact support")
+def randomnumber(length: int):
+    """
+    Generate a random number.\n
+    Parameters:\n
+    • length: The length of the number you want to generate\n\n
+    """
+    url=f"https://api.sillychat.tech/randomnumber?length={length}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        if result[0]['Error'] == 'True':
+            raise Exception(result[0]['ErrorInfo'])
+        else:
+            return result[0]['number']
+    raise Exception("Unknown error occured, please contact support")
+def randomtopic():
+    """
+    Generate a random topic.
+    """
+    url=f"https://api.sillychat.tech/randomtopic"
+    response = requests.get(url)
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        if result[0]['Error'] == 'True':
+            raise Exception(result[0]['ErrorInfo'])
+        else:
+            return result[0]['topic']
+    raise Exception("Unknown error occured, please contact support")
