ab7d58ce41353562f04a8866c0322c16d552c297
thegamer3514 <thegamer3514@gmail.com>
2023-06-03 18:56:49 +0100

    push

---
+++ README.md
@@ -0,0 +1,6 @@
+# Gamer3514
+A small package that does stuff
+## Installation
+```bash
+pip install gamer3514
+```
---
+++ src/gamer3514/api.py
@@ -0,0 +1,38 @@
+import requests
+import json
+def detectnsfw(imageurl: str, apikey: str):
+    """
+    Detect NSFW content in images.\n
+    Parameters:\n
+    • imageurl: The url of the image you want to scan\n
+    • apikey: Your API key. You can get one at https://api.sillychat.tech\n\n
+
+    """
+    url=f"https://api.sillychat.tech/detectnsfw?url={imageurl}&key={apikey}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        try:
+            result['data']
+            return result
+        except:
+            raise Exception(result[0]['ErrorInfo'])
+    else:
+        raise Exception("Unknown error occured, please contact support")
+def profanitycheck(text: str, apikey: str):
+    """
+    Check if a text contains profanity.\n
+    Parameters:\n
+    • text: The text you want to scan\n
+    • apikey: Your API key. You can get one at https://api.sillychat.tech\n\n
+    """
+    url=f"https://api.sillychat.tech/profanitycheck?key={apikey}&text={text}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        result = json.loads(response.text)
+        if result[0]['Error'] == 'True':
+            raise Exception(result[0]['ErrorInfo'])
+        else:
+            return result[0]['Profanity']
+    raise Exception("Unknown error occured, please contact support")
+print(profanitycheck("WOW", "jbdanowdlekkmtvbsrlkdphv"))
