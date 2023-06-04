7720386feddc97b8afa6ebaf226537e60f6e8649
thegamer3514 <thegamer3514@gmail.com>
2023-06-04 12:21:59 +0100

    new update

--- src/gamer3514/api.py
+++ src/gamer3514/api.py
@@ -78,3 +78,62 @@ def randomtopic():
         else:
             return result[0]['topic']
     raise Exception("Unknown error occured, please contact support")
+def cat(filepath: str, apikey: str):
+    """
+    Generate a cat image.\n
+    Parameters:\n
+    • filepath: The path of the image you want to save\n
+    • apikey: Your API key. You can get one at https://api.sillychat.tech\n\n
+    """
+    url=f"https://api.sillychat.tech/cat?key={apikey}"
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+        return True
+    raise Exception("Unknown error occured, please contact support")
+def catgif(filepath: str, apikey: str):
+    """
+    Generate a cat gif.\n
+    Parameters:\n
+    • filepath: The path of the gif you want to save\n
+    • apikey: Your API key. You can get one at https://api.sillychat.tech\n\n
+    """
+    url=f"https://api.sillychat.tech/catgif?key={apikey}"
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        if filepath.endswith(".gif"):
+            with open(filepath, 'wb') as f:
+                f.write(response.content)
+            return True
+        else:
+            raise Exception("Filepath must end with .gif")
+    raise Exception("Unknown error occured, please contact support")
+def dog(filepath: str, apikey: str):
+    """
+    Generate a dog image.\n
+    Parameters:\n
+    • filepath: The path of the image you want to save\n
+    • apikey: Your API key. You can get one at https://api.sillychat.tech\n\n
+    """
+    url=f"https://api.sillychat.tech/dog?key={apikey}"
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+        return True
+    raise Exception("Unknown error occured, please contact support")
+def donaldtweet(filepath: str, text: str):
+    """
+    Generate a Donald Trump tweet.\n
+    Parameters:\n
+    • filepath: The path of the image you want to save\n
+    • text: The text you want to put in the tweet\n\n
+    """
+    url=f"https://api.sillychat.tech/tweet?text={text}"
+    response = requests.get(url, stream=True)
+    if response.status_code == 200:
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+        return True
+    raise Exception("Unknown error occured, please contact support")
